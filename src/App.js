e8674e04f05638e03a1f7b1967b2509d5be034d2
FirstBoy1 <zeems1234@gmail.com>
2020-09-10 00:47:42 +0500

    added bubble, selection, insertion sort algo

--- src/App.js
+++ src/App.js
@@ -34,15 +34,11 @@ function App() {
       const { comparison, swap } = animations[i];
       console.log(comparison, swap);
       setTimeout(() => {
-        // arrayBars[comparison[0]].style.backgroundColor = 'red';
-        // arrayBars[comparison[1]].style.backgroundColor = 'red';
         setTimeout(() => {
           arrayBars[swap[0]].style.height = `${array[swap[0]]}px`;
           arrayBars[swap[1]].style.height = `${array[swap[1]]}px`;
           arrayBars[swap[0]].textContent = `${array[swap[0]]}`;
           arrayBars[swap[1]].textContent = `${array[swap[1]]}`;
-          // arrayBars[swap[0]].style.backgroundColor = 'green';
-          // arrayBars[swap[1]].style.backgroundColor = 'green';
         }, i * 10);
       }, i * 10);
     }
@@ -50,7 +46,66 @@ function App() {
 
   const quickSort = () => {};
   const heapSort = () => {};
-  const bubbleSort = () => {};
+
+  const bubbleSort = () => {
+    const animations = sortingAlgorithms.bubbleSort(array);
+    const arrayBars = document.querySelectorAll('.arrayContainer__bar');
+    for (let i = 0; i < animations.length; i++) {
+      const { /* comparison, */ swap } = animations[i];
+      setTimeout(() => {
+        setTimeout(() => {
+          if (swap) {
+            console.log(array[swap[0]], array[swap[1]]);
+            arrayBars[swap[0]].style.height = `${array[swap[0]]}px`;
+            arrayBars[swap[1]].style.height = `${array[swap[1]]}px`;
+            arrayBars[swap[0]].textContent = `${array[swap[0]]}`;
+            arrayBars[swap[1]].textContent = `${array[swap[1]]}`;
+          } else {
+          }
+        }, i * 10);
+      }, i * 10);
+    }
+  };
+
+  const insertionSort = () => {
+    const animations = sortingAlgorithms.insertionSort(array);
+    const arrayBars = document.querySelectorAll('.arrayContainer__bar');
+    for (let i = 0; i < animations.length; i++) {
+      const { /* comparison, */ swap } = animations[i];
+      setTimeout(() => {
+        setTimeout(() => {
+          if (swap) {
+            console.log(array[swap[0]], array[swap[1]]);
+            arrayBars[swap[0]].style.height = `${array[swap[0]]}px`;
+            arrayBars[swap[1]].style.height = `${array[swap[1]]}px`;
+            arrayBars[swap[0]].textContent = `${array[swap[0]]}`;
+            arrayBars[swap[1]].textContent = `${array[swap[1]]}`;
+          } else {
+          }
+        }, i * 10);
+      }, i * 10);
+    }
+  };
+
+  const selectionSort = () => {
+    const animations = sortingAlgorithms.selectionSort(array);
+    const arrayBars = document.querySelectorAll('.arrayContainer__bar');
+    for (let i = 0; i < animations.length; i++) {
+      const { /* comparison, */ swap } = animations[i];
+      setTimeout(() => {
+        setTimeout(() => {
+          if (swap) {
+            console.log(array[swap[0]], array[swap[1]]);
+            arrayBars[swap[0]].style.height = `${array[swap[0]]}px`;
+            arrayBars[swap[1]].style.height = `${array[swap[1]]}px`;
+            arrayBars[swap[0]].textContent = `${array[swap[0]]}`;
+            arrayBars[swap[1]].textContent = `${array[swap[1]]}`;
+          } else {
+          }
+        }, i * 10);
+      }, i * 10);
+    }
+  };
 
   return (
     <div className="app">
@@ -67,6 +122,8 @@ function App() {
         <button onClick={() => quickSort()}>Quick Sort</button>
         <button onClick={() => heapSort()}>Heap Sort</button>
         <button onClick={() => bubbleSort()}>Bubble Sort</button>
+        <button onClick={() => insertionSort()}>Insertion Sort</button>
+        <button onClick={() => selectionSort()}>Selection Sort</button>
       </div>
       <div className="arrayContainer">
         {array.map((value, idx) => (
--- src/sortingAlgorithms.js
+++ src/sortingAlgorithms.js
@@ -1,3 +1,52 @@
+// Bubble Sort
+export function bubbleSort(arr) {
+  const animations = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      const animation = { comparison: [i, j] };
+      if (arr[j] < arr[i]) {
+        animation.swap = [i, j];
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+      animations.push(animation);
+    }
+  }
+
+  return animations;
+}
+
+// Insertion Sort
+export function insertionSort(arr) {
+  const animations = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    let j = i;
+    while (arr[j] < arr[j - 1]) {
+      animations.push({ comparison: [j, j - 1], swap: [j, j - 1] });
+      [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+      j -= 1;
+    }
+  }
+
+  return animations;
+}
+
+// Selection Sort
+export function selectionSort(arr) {
+  const animations = [];
+  for (let i = 0; i < arr.length; i++) {
+    let swapIdx = i;
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] < arr[swapIdx]) swapIdx = j;
+    }
+    animations.push({ swap: [i, swapIdx] });
+    [arr[i], arr[swapIdx]] = [arr[swapIdx], arr[i]];
+  }
+
+  return animations;
+}
+
+// Merge Sort
 export function mergeSort(arr) {
   if (arr.length <= 1) return arr;
   const animations = [];
